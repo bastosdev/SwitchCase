96b758d395d745f2bdaf28518fef5b2580ea22b9
Bruno Bastos <bastosdev@outlook.com>
2021-03-26 20:48:04 -0300

    Adicionar arquivos de projeto.

---
+++ SwitchCase/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwitchCase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cboSemana_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cboSemana.Text)
+            {
+                case "Domingo":
+                    MessageBox.Show("1º dia da Semana!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Segunda":
+                    MessageBox.Show("2º dia da Semana!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Terça":
+                    MessageBox.Show("3º dia da Semana!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Quarta":
+                    MessageBox.Show("4º dia da Semana!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Quinta":
+                    MessageBox.Show("5º dia da Semana!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Sexta":
+                    MessageBox.Show("6º dia da Semana!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Sábado":
+                    MessageBox.Show("7º dia da Semana!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+            }
+        }
+    }
+}
